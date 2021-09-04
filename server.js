72a69113f1fa916eb31814f826be52102d43d0e2
mehrab656 <azmir15-849@diu.edu.bd>
2021-09-04 12:35:45 +0600

    Mongodb install and connect to database

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv =  require('dotenv');
 const morgan = require('morgan');
-
+const connectDB  = require('./config/db')
 
 //Route files
 const bootcamps = require('./routes/bootcamps')
 
-
 //Load env vars/load config files
 dotenv.config({path:'./config/config.env'});
+
+//Connect to database 
+connectDB();
 
 const app =  express();
 
@@ -26,4 +28,11 @@ const PORT =  process.env.PORT || 5000;
 
 
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
+//Handle unhandle promise rejections
+process.on('unHandleRejection',(err,promise)=>{
+    console.log(`Error : ${err.message}`);
+    //close server and exit process
+    server.close(()=>process.exit(1));
+})
