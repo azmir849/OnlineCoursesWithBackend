d0b3019c5743a083606539d01ecd27f55540bce9
mehrab656 <azmir15-849@diu.edu.bd>
2021-09-04 13:11:08 +0600

    Colors install

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
             
     });
 
-    console.log(`mongodb connected : ${conn.connection.host}`)
+    console.log(`mongodb connected : ${conn.connection.host}`.cyan.underline.bold)
 }
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv =  require('dotenv');
 const morgan = require('morgan');
+const colors  = require('colors')
 const connectDB  = require('./config/db')
 
 //Route files
@@ -28,11 +29,11 @@ const PORT =  process.env.PORT || 5000;
 
 
 
-const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 //Handle unhandle promise rejections
 process.on('unHandleRejection',(err,promise)=>{
-    console.log(`Error : ${err.message}`);
+    console.log(`Error : ${err.message}`.red);
     //close server and exit process
     server.close(()=>process.exit(1));
 })
