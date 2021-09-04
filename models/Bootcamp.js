49b292b7ab56b4707388338d170a9ebbd1a1597e
mehrab656 <azmir15-849@diu.edu.bd>
2021-09-04 17:58:21 +0600

    Bootcamp GET AND POST method created

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,42 +1,44 @@
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 
-const BootcampSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required:[true,'Please add a name'],
-        unique:true,
-        trim:true,
-        maxlength:[50, 'Name can not be more than 50 characters']
+
+const BootcampSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+      unique: true,
+      trim: true,
+      maxlength: [50, 'Name can not be more than 50 characters']
     },
     slug: String,
-     description:{
-        type: String,
-        required:[true,'Please add a description'],
-        maxlength:[500, 'Description can not be more than 500 characters']
+    description: {
+      type: String,
+      required: [true, 'Please add a description'],
+      maxlength: [500, 'Description can not be more than 500 characters']
     },
     website: {
-        type:String,
-        match:[
-           /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-           'Please  use a valid URL with HTTP OR HTTPS'
-        ]
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please use a valid URL with HTTP or HTTPS'
+      ]
     },
     phone: {
-        type:String,
-        maxlength:[20,'Phone number can not be longer than 20 characters']
+      type: String,
+      maxlength: [20, 'Phone number can not be longer than 20 characters']
     },
-    email :{
-         type : String,
-         match :[
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid email'
-         ]
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please add a valid email'
+      ]
     },
-    address:{
-        type:String,
-        required:[true, 'Please add an address']
+    address: {
+      type: String,
+      required: [true, 'Please add an address']
     },
-     location: {
+    location: {
       // GeoJSON Point
       type: {
         type: String,
@@ -53,7 +55,7 @@ const BootcampSchema = new mongoose.Schema({
       zipcode: String,
       country: String
     },
-     careers: {
+    careers: {
       // Array of strings
       type: [String],
       required: true,
@@ -66,7 +68,7 @@ const BootcampSchema = new mongoose.Schema({
         'Other'
       ]
     },
-     averageRating: {
+    averageRating: {
       type: Number,
       min: [1, 'Rating must be at least 1'],
       max: [10, 'Rating must can not be more than 10']
@@ -96,16 +98,16 @@ const BootcampSchema = new mongoose.Schema({
       type: Date,
       default: Date.now
     },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
-    }
-}, 
-{
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  }
+    // user: {
+    //   type: mongoose.Schema.ObjectId,
+    //   ref: 'User',
+    //   required: true
+    // }
+  },
+//   {
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true }
+//   }
 );
 
-module.exports = mongoose.model('Bootcamp',BootcampSchema)
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
