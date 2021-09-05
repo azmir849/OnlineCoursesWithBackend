80df3ca174b40c453269ed4c3266bc6bef8b4241
mehrab656 <azmir15-849@diu.edu.bd>
2021-09-05 12:08:29 +0600

    Mongoose duplicate key and validation error verification created

--- middleware/error.js
+++ middleware/error.js
@@ -6,7 +6,7 @@ const errorHandler = (err,req,res,next)=>{
     error.message = err.message;
 
     //Log to console for dev
-    console.log(err.stack.red);
+    console.log(err);
 
     //Mongoose bad ObjectId
     console.log(err.name)
@@ -14,6 +14,19 @@ const errorHandler = (err,req,res,next)=>{
         const message = `Bootcamp not found with id of ${err.value}`;
         error = new ErrorResponse(message,404)
     }
+
+    //Mongoose duplicate key
+    if(err.code === 11000){
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message , 400);
+    }
+
+    //Mongoose validation error
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
 
     res.status(error.statusCode || 500).json({
         success:false,
