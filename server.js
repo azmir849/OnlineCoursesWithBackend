047a7fb71e499cdff6086bd6b33a0655026fabd3
mehrab656 <azmir15-849@diu.edu.bd>
2021-09-05 10:56:13 +0600

    Create error  middleware for  custome error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv =  require('dotenv');
 const morgan = require('morgan');
 const colors  = require('colors')
+const errorHandler =  require('./middleware/error')
 const connectDB  = require('./config/db')
 
 //Route files
@@ -27,6 +28,7 @@ if(process.env.NODE_ENV==='development'){
 //Mount Routers
 app.use('/api/v1/bootcamps',bootcamps)
 
+app.use(errorHandler);
 
 const PORT =  process.env.PORT || 5000;
 
