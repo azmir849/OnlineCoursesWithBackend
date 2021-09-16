e9c74d24a5f0589315abfc84d4c741ba2d6718f9
azmir849 <azmir15-849@diu.edu.bd>
2021-09-16 17:47:35 +0600

    Sending JWT in a Cookie

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const dotenv =  require('dotenv');
 const morgan = require('morgan');
 const fileupload = require('express-fileupload');
-const colors  = require('colors')
-const errorHandler =  require('./middleware/error')
-const connectDB  = require('./config/db')
+const cookieParser = require('cookie-parser');
+const colors  = require('colors');
+const errorHandler =  require('./middleware/error');
+const connectDB  = require('./config/db');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -21,7 +22,10 @@ connectDB();
 const app =  express();
 
 //Body parser
-app.use(express.json())
+app.use(express.json());
+
+//Cookie-parser
+app.use(cookieParser());
 
 //Dev logging middleware
 if(process.env.NODE_ENV==='development'){
