268333e4d2290a1c5efcbaca27956fd36273ed8c
azmir849 <azmir15-849@diu.edu.bd>
2021-09-28 13:30:16 +0600

    Course ownership created successfully

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,122 +1,148 @@
-const Course  = require('../models/Course');
-const Bootcamp = require('../models/Bootcamp');
-const asyncHandler =  require('../middleware/async');
-const ErrorResponse =  require('../utils/errorResponse');
-
+const Course = require("../models/Course");
+const Bootcamp = require("../models/Bootcamp");
+const asyncHandler = require("../middleware/async");
+const ErrorResponse = require("../utils/errorResponse");
 
 //@desc     Get all courses
 //@route    GET /api/v1/courses
 //@route    GET /api/v1/bootcamps/:bootcampId/courses
 //@access   Public
 
-exports.getCourses = asyncHandler(async(req,res,next)=>{
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  if (req.params.bootcampId) {
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
 
-    if(req.params.bootcampId){
-        const courses = await Course.find({bootcamp:req.params.bootcampId});
-
-        return res.status(200).json({
-            success:true,
-            count:courses.length,
-            data: courses
-        });
-
-    }else{
-        res.status(200).json(res.advancedResults);
-    }
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses,
+    });
+  } else {
+    res.status(200).json(res.advancedResults);
+  }
 });
-
-
 
 //@desc     Get Single course
-//@route    GET /api/v1/courses/:id  
+//@route    GET /api/v1/courses/:id
 //@access   Public
 
-exports.getCourse = asyncHandler(async(req,res,next)=>{
-   
-    const course = await Course.findById(req.params.id).populate({
-        path:'bootcamp',
-        select:'name description'
-    });
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp",
+    select: "name description",
+  });
 
-    if(!course){
-        return next(new ErrorResponse(`No course with the  id of ${req.params.id}`),404);
-    }
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the  id of ${req.params.id}`),
+      404
+    );
+  }
 
-    res.status(200).json({
-        success:true,
-        data:course
-    })
-
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
 });
-
-
 
 //@desc     Add course
 //@route    Post /api/v1/bootcamps/:bootcampId/courses
 //@access   Private
 
-exports.addCourse = asyncHandler(async(req,res,next)=>{
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user.id;
 
-    req.body.bootcamp = req.params.bootcampId;
-   
-    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
-    if(!bootcamp){
-        return next(new ErrorResponse(`No bootcamp with the  id of ${req.params.bootcampId}`),404);
-    }
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with the  id of ${req.params.bootcampId}`),
+      404
+    );
+  }
 
-    const course = await Course.create(req.body);
+  //Make sure user is course owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `This user with this id ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
 
-    res.status(200).json({
-        success:true,
-        data:course
-    })
+  const course = await Course.create(req.body);
 
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
 });
-
 
 //@desc     Update course
 //@route    PUT/api/v1/courses/:id
 //@access   Private
 
-exports.updateCourse = asyncHandler(async(req,res,next)=>{
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
 
-    let course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course find with the  id of ${req.params.id}`),
+      404
+    );
+  }
 
-    if(!course){
-        return next(new ErrorResponse(`No course find with the  id of ${req.params.id}`),404);
-    }
+  //Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `This user with this id ${req.user.id} is not authorized to update a course of id ${course._id}`,
+        401
+      )
+    );
+  }
 
-    course = await Course.findByIdAndUpdate(req.params.id, req.body,{
-        new: true,
-        runValidators:true,
-    })
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    res.status(200).json({
-        success:true,
-        data:course
-    })
-
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
 });
-
 
 //@desc     Delete course
 //@route    Delete/api/v1/courses/:id
 //@access   Private
 
-exports.deleteCourse = asyncHandler(async(req,res,next)=>{
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
 
-   const course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course find with the  id of ${req.params.id}`),
+      404
+    );
+  }
 
-    if(!course){
-        return next(new ErrorResponse(`No course find with the  id of ${req.params.id}`),404);
-    }
+  //Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `This user with this id ${req.user.id} is not authorized to delete a course of id ${course._id}`,
+        401
+      )
+    );
+  }
 
-   await course.remove();
+  await course.remove();
 
-    res.status(200).json({
-        success:true,
-        data:{}
-    })
-
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 });
