b5d27833198b8493159cf989c2e80b07bc4fa70e
azmir849 <azmir15-849@diu.edu.bd>
2021-09-28 17:28:49 +0600

    Forgot password token and expire time set

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,87 +1,103 @@
-const ErrorResponse =  require('../utils/errorResponse');
-const asyncHandler =  require('../middleware/async');
-const User = require('../models/User');
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const User = require("../models/User");
 
 //@desc     Register user
 //@route    POST /api/v1/auth/register
 //@access   Public
-exports.register = asyncHandler(async (req, res, next)=>{
-   const {name,email,password,role} = req.body;
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
 
-   //Create user
-   const user = await User.create({
-        name,
-        email,
-        password,
-        role
-   });
+  //Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
 
-   sendTokenResponse(user, 200, res);
+  sendTokenResponse(user, 200, res);
 });
-
-
 
 //@desc     Login user
 //@route    GET /api/v1/auth/login
 //@access   Public
-exports.login = asyncHandler(async (req, res, next)=>{
-    const {email,password} = req.body;
- 
-    //Validate email and password
-    if(!email || !password){
-        return next(new ErrorResponse('Please provide an email and password',400));
-    }
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    //Check for user
-    const user = await User.findOne({ email }).select('+password');
-    if(!user){
-        return next(new ErrorResponse('Invalid credentials',401));
-    }
+  //Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
 
-    //  Check if password matches
-    const isMatch = await user.matchPassword(password);
+  //Check for user
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-    if(!isMatch){
-        return next(new ErrorResponse('Invalid credentials',401));
-    }
-    
- 
-   sendTokenResponse(user, 200, res);
- });
- 
+  //  Check if password matches
+  const isMatch = await user.matchPassword(password);
 
- //Get token from model, create cookie and send response
- const sendTokenResponse = (user,statusCode, res)=>{
-     // Create token
-    const token = user.getSignedJwtToken();
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
-    const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly: true
-    };
-
-    if(process.env.NODE_ENV === 'production'){
-        options.secure = true;
-    }
-
-    res
-    .status(statusCode)
-    .cookie('token',token, options)
-    .json({
-        success:true,
-        token
-    })
- }
+  sendTokenResponse(user, 200, res);
+});
 
 //@desc     Get current looged in user
 //@route    GET /api/v1/auth/me
 //@access   Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
-exports.getMe = asyncHandler(async (req, res, next)=>{
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: user
-    });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
+
+//@desc     Forgot password
+//@route    GET /api/v1/auth/forgotpassword
+//@access   Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse("There is no user with that email", 404));
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+  //console.log(resetToken);
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+//Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+  });
+};
