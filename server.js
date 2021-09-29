39fa0c54d9c208357dfbd9d6cdf9fd5f999d2969
azmir849 <azmir15-849@diu.edu.bd>
2021-09-29 17:18:31 +0600

    CRUD by Admin for Users done

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-const path = require('path');
-const express = require('express');
-const dotenv =  require('dotenv');
-const morgan = require('morgan');
-const fileupload = require('express-fileupload');
-const cookieParser = require('cookie-parser');
-const colors  = require('colors');
-const errorHandler =  require('./middleware/error');
-const connectDB  = require('./config/db');
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const fileupload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const colors = require("colors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 //Route files
-const bootcamps = require('./routes/bootcamps');
-const courses = require('./routes/courses');
-const auth = require('./routes/auth');
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 //Load env vars/load config files
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
-//Connect to database 
+//Connect to database
 connectDB();
 
-const app =  express();
+const app = express();
 
 //Body parser
 app.use(express.json());
@@ -28,33 +29,36 @@ app.use(express.json());
 app.use(cookieParser());
 
 //Dev logging middleware
-if(process.env.NODE_ENV==='development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //File uploading
 app.use(fileupload());
 
 //Set  static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //Mount Routers
-app.use('/api/v1/bootcamps',bootcamps);
-app.use('/api/v1/courses',courses);
-app.use('/api/v1/auth',auth);
+app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
 
 app.use(errorHandler);
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-
-const server = app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
 
 //Handle unhandle promise rejections
-process.on('unHandleRejection',(err,promise)=>{
-    console.log(`Error : ${err.message}`.red);
-    //close server and exit process
-    server.close(()=>process.exit(1));
-})
+process.on("unHandleRejection", (err, promise) => {
+  console.log(`Error : ${err.message}`.red);
+  //close server and exit process
+  server.close(() => process.exit(1));
+});
